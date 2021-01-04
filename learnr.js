410d9b6c0b8f35fdc2520e129e083302074b8785
Lydia Ross <lydiamross@live.com>
2021-01-04 16:23:12 +0000

    Adds util for static files

--- learnr.js
+++ learnr.js
@@ -1,22 +1,34 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const http = require('http');
 const port = process.env.PORT || 3000;
+
+const serveStaticFile = (res, path, contentType, responseCode = 200) => {
+    fs.readFile(__dirname + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain' });
+            return res.end(`500 - Internal error, ${err}`);
+        };
+        res.writeHead(responseCode, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
 
 const server = http.createServer((req, res) => {
     const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch(path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png');
             break;
         default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not found');
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
     };
 });

c68f4e261b4429221078cdc51dbeb2fd0ca73ff4
Lydia Ross <lydiamross@live.com>
2021-01-04 15:51:59 +0000

    Introduces switch on paths

--- learnr.js
+++ learnr.js
@@ -4,8 +4,21 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world!');
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(path) {
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not found');
+            break;
+    };
 });
 
 server.listen(port, () => {

8869ba41e863ebec605a8dc76e3f982abc15aa59
Lydia Ross <lydiamross@live.com>
2021-01-04 16:31:13 +0000

    feat: Serves html pages (11) (#12)

--- learnr.js
+++ learnr.js
@@ -1,11 +1,36 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const serveStaticFile = (res, path, contentType, responseCode = 200) => {
+    fs.readFile(__dirname + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain' });
+            return res.end(`500 - Internal error, ${err}`);
+        };
+        res.writeHead(responseCode, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world!');
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
+            break;
+    };
 });
 
 server.listen(port, () => {

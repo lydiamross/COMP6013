09efc6127e8eedf04a6e5dc5ee74c56779f27a7c
Lydia Ross <lydiamross@live.com>
2021-01-04 11:30:28 +0000

    feat: Creates server and documentation (9) (#10)

---
+++ learnr.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello world!');
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}, press Ctrl-C to terminate...`);
+});

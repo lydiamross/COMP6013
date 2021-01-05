c7abae40779890a2799769af144229474dc493e3
Lydia Ross <lydiamross@live.com>
2021-01-05 17:30:33 +0000

    feat: Improves quality and maintainability of codebase (14) (#16)

--- learnr.js
+++ learnr.js
@@ -1,38 +1,25 @@
 require('dotenv').config();
-
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const handlers = require('./lib/handlers');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main',
-}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(express.static(`${__dirname}/public`));
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.get('/', handlers.home);
 
-app.use(express.static(__dirname + '/public'))
+app.get('/about', handlers.about);
 
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-    console.error(`Error - ${err.message}`);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}, press Ctrl-C to terminate...`);
+  console.log(`Server started on port ${port}, press Ctrl-C to terminate...`);
 });

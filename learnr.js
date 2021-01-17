b4b42a59ec758665d78179853c68cef99481b3ff
Lydia Ross <lydiamross@live.com>
2021-01-17 22:56:12 +0000

    refactor: Moves routes to separate file (#30)

--- learnr.js
+++ learnr.js
@@ -3,18 +3,17 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
-const handlers = require('./lib/handlers');
 const { credentials } = require('./config');
+const addRoutes = require('./routes');
 require('./db');
 
 const app = express();
+addRoutes(app);
 const port = process.env.PORT || 3000;
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
-
-app.use(express.static(`${__dirname}/public`));
 
 app.use(cookieParser(credentials.cookieSecret));
 
@@ -23,20 +22,6 @@ app.use(expressSession({
   saveUninitialized: false,
   secret: credentials.cookieSecret,
 }));
-
-app.get('/', handlers.home);
-
-app.get('/about', handlers.about);
-
-app.get('/topic', handlers.listTopics);
-
-app.get('/card', handlers.listCards);
-
-app.get('/account', handlers.account);
-
-app.use(handlers.notFound);
-
-app.use(handlers.serverError);
 
 if (require.main === module) {
   app.listen(port, () => {

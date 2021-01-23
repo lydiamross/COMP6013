9ebf8d8e4815769df005fa74391f4218a95e42bd
Lydia Ross <lydiamross@live.com>
2021-01-23 13:42:17 +0000

    Removes express-session and cookie-parser

--- server/src/learnr.js
+++ server/src/learnr.js
@@ -1,21 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
 const addRoutes = require('./routes');
 require('./db');
 
 const app = express();
 addRoutes(app);
 const port = process.env.PORT || 3000;
-
-app.use(cookieParser(process.env.COOKIE_SECRET));
-
-app.use(expressSession({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-}));
 
 if (require.main === module) {
   app.listen(port, () => {

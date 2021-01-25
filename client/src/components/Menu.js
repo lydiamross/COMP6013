41cb51b92497d7202db598557f1714a371012dec
Lydia Ross <lydiamross@live.com>
2021-01-25 19:22:24 +0000

    Adds fontawesome

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const Menu = () => {
   return (
     <nav>
+      <FontAwesomeIcon icon={faBars} />
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>

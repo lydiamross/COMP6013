fe000b22766db223011374668aaf0e2411f58a8c
Lydia Ross <lydiamross@live.com>
2021-01-26 09:29:35 +0000

    Adds state to Menu for change display on click

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,17 +1,30 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
 
 export const Menu = () => {
+  const [isMenuDisplayed, setMenuDisplay] = useState(true);
+
+  function onToggleChange() {
+    setMenuDisplay(!isMenuDisplayed);
+  }
+
   return (
     <nav>
-      <FontAwesomeIcon icon={faBars} />
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/cards">Cards</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
-    </nav>
+      <div>
+        <FontAwesomeIcon
+          onClick={onToggleChange}
+          icon={faBars} />
+      </div>
+      {isMenuDisplayed &&
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/cards">Cards</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+      }
+    </nav >
   );
 };

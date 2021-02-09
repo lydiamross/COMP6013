a5ab75a4858903f473de9c62cee0342a28a179f4
Lydia Ross <lydiamross@live.com>
2021-02-09 02:17:24 +0000

    Combines App and index files

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import logo from './logo.png';
+import { Cards } from './components/Cards.js';
+import { Topics } from './components/Topics.js';
+import { About } from './components/About';
+import { NotFound } from './components/NotFound';
+import { Menu } from './components/Menu';
+import { AppContainer, Logo, Header, Section } from './styled';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer>
+      <Router>
+        <Header>
+          <Link to="/"><Logo src={logo} alt="Logo" /></Link>
+          {/* <h1>Learnr</h1> */}
+        </Header>
+        <Menu />
+        <Section>
+          <Switch>
+            <Route path="/" exact component={Topics} />
+            <Route path="/about" exact component={About} />
+            <Route path="/cards" exact component={Cards} />
+            <Route component={NotFound} />
+          </Switch>
+        </Section>
+      </Router>
+    </AppContainer>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

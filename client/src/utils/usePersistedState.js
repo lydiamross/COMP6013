65c7886b7d1731ab2646c958e190088285d84aa0
Lydia Ross <lydiamross@live.com>
2021-03-01 22:35:33 +0000

    Adds changes

--- client/src/utils/usePersistedState.js
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from 'react';
-
-export const usePersistedState = (key, defaultValue) => {
-  const [state, setState] = useState(localStorage.getItem(key) || defaultValue);
-  useEffect(() => { localStorage.setItem(key, state); }, [key, state]);
-  return [state, setState];
-};
